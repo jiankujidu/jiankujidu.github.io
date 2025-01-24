73fc81032857f01cb01ccb0e8b2f5247ba3576b5
冬涛觞雪 <dzt20081208@outlook.com>
2025-01-24 20:28:56 +0800

    Site updated: 2025-01-24 20:28:55

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1737721565818";
+const VERSION = "1737721727700";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
